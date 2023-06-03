8a297ac2f9791da7ed623b03cae9eab51e534cf9
Garv nanwani <garvwashere@gmail.com>
2023-06-03 20:39:26 +0530

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Garv Nanwani on 03/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
